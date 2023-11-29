500ecc2e3e4d3a4eae091e6f57cabd75196f418e
woyue2 <stephendu@foxmail.com>
2023-11-30 02:15:22 +0800

    Update main.py

--- code_ps1/main.py
+++ code_ps1/main.py
@@ -1,42 +1,38 @@
-import sys
-import peak
-import trace
-import algorithms
-import json
-import utils
+import sys  # 导入sys模块，用于处理Python运行时的环境（命令行参数等）
+import peak  # 导入peak模块，用于处理峰值问题
+import trace  # 导入trace模块，用于跟踪算法的执行
+import algorithms  # 导入algorithms模块，包含各种算法的实现
+import json  # 导入json模块，用于处理JSON数据
+import utils  # 导入utils模块，包含各种实用工具函数
 
-################################################################################
-################################ The Main Method ###############################
-################################################################################
+# 以下是主函数的定义
 
 def loadProblem(file = "problem.py", variable = "problemMatrix"):
     """
-    Loads a matrix from a python file, and constructs a PeakProblem from it.
+    从python文件中加载矩阵，并根据它构建PeakProblem对象。
     """
+    namespace = dict()  # 创建一个空字典，用于存储执行python文件后的命名空间
+    with open(file) as handle:  # 打开文件
+        exec(handle.read(), namespace)  # 执行文件中的python代码
+    return peak.createProblem(namespace[variable])  # 从命名空间中获取矩阵，并创建PeakProblem对象
 
-    namespace = dict()
-    with open(file) as handle:
-        exec(handle.read(), namespace)
-    return peak.createProblem(namespace[variable])
+def main():  # 定义主函数
+    if len(sys.argv) > 1:  # 如果命令行参数数量大于1（除了脚本名之外还有其他参数）
+        problem = loadProblem(sys.argv[1])  # 使用第一个命令行参数作为文件名，加载问题
+    else:  # 如果没有其他命令行参数
+        problem = loadProblem(utils.getOpenFilename("problem.py"))  # 使用用户选择的文件名，加载问题
 
-def main():
-    if len(sys.argv) > 1:
-        problem = loadProblem(sys.argv[1])
-    else:
-        problem = loadProblem(utils.getOpenFilename("problem.py"))
-
-    # run all algorithms, gathering the traces and printing out the results as
-    # we go
+    # 运行所有算法，收集跟踪信息并打印结果
     algorithmList = [("Algorithm 1", algorithms.algorithm1),
                      ("Algorithm 2", algorithms.algorithm2),
                      ("Algorithm 3", algorithms.algorithm3),
-                     ("Algorithm 4", algorithms.algorithm4)]
+                     ("Algorithm 4", algorithms.algorithm4)]  # 定义算法列表
 
-    steps = []
+    steps = []  # 创建一个空列表，用于存储每个算法的步骤数
     
-    for (name, function) in algorithmList:
-        tracer = trace.TraceRecord()
-        peak = function(problem, trace = tracer)
+    for (name, function) in algorithmList:  # 遍历算法列表
+        tracer = trace.TraceRecord()  # 创建一个TraceRecord对象，用于跟踪算法的执行
+        peak = function(problem, trace = tracer)  # 执行算法，并传入TraceRecord对象
         steps.append(tracer.sequence)
         
         status = "is NOT a peak (INCORRECT!)"
@@ -45,7 +41,7 @@ def main():
 
         print(name + " : " + str(peak) + " => " + status)
 
-    # write the trace out to a file
+    # 将跟踪信息写入文件
     with open("trace.jsonp", "w") as traceFile:
         traceFile.write("parse(")
 
